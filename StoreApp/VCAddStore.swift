8a2d964691fc29bf2d5a1b27185f6ead86330229
Mohammad Al-Fauri <mam.alfauri@gmail.com>
2023-04-10 23:41:06 +0300

    1

---
+++ StoreApp/VCAddStore.swift
@@ -0,0 +1,29 @@
+//
+//  VCAddStore.swift
+//  StoreApp
+//
+//  Created by Mohammad Alfauri on 10/04/2023.
+//
+
+import UIKit
+
+class VCAddStore: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
